13ec666b01402ed7774e6d0df92989cb7fce55fe
Hamza Moumni <94747@supinfo.com>
2015-10-21 10:59:11 -0700

    Explaining Viewstate in a great detail

---
+++ ViewState/ViewState/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewState
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public int click_count = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                TextBox1.Text = "0";
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            click_count = click_count + 1;
+            TextBox1.Text = click_count.ToString();
+
+        }
+    }
+}
---
+++ ViewState/ViewState/WebForm2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewState
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        int click = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TextBox1.Text = "0";
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+              if (ViewState["Clicks"] != null)
+            {
+                click = (int)ViewState["Clicks"] + 1;
+            }
+            TextBox1.Text = click.ToString();
+            ViewState["Clicks"] = click;
+        }
+    }
+}
